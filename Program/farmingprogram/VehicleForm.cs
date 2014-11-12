088e7650d702d39379bc9a9c467c7c66319231e9
Mark <venousdegree@gmail.com>
2014-11-12 16:33:51 +0000

    Vehicle Class
    Class with the constants and a design of a form

---
+++ Program/farmingprogram/VehicleForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehicle_Class
+{
+    public partial class VehicleForm : Form
+    {
+
+        public VehicleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.textBox1 = textBox1.Text;
+        }
+    }
+}
