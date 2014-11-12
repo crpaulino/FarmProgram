19901cd4ea7f985fd883ae06deecc72e3e5ff757
thomasb9 <thomasjbarry9@yahoo.co.uk>
2014-11-12 16:48:33 +0000

    Field and Fertiliser classes

---
+++ Program/farmingprogram/Fertiliser.cs
@@ -0,0 +1,53 @@
+﻿//Coded by SID 1335132
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertiliserProgram
+{
+    class Fertiliser
+    {
+        //Attributes
+        public int FertilizerID;
+        private string fertName;
+        private string fertDose;
+        private string fertNote; 
+
+        public Fertiliser(int fertID, string fertname, string fertdose, string fertnote)
+        {
+           //constructors
+            this.fertName = fertname;
+            this.FertilizerID = fertID;
+            this.fertDose = fertdose;
+            this.fertNote = fertnote;
+
+        }
+        //properties
+        public string FertDose
+        {
+            get { return fertDose; }
+            set { fertDose = value; }
+        }
+    
+        public string FertName
+        {
+            get { return fertName; }
+            set { fertName = value; }
+        }
+
+        private int FertiliserID 
+        {
+            get { return FertilizerID; }
+            set { FertilizerID = value; }   
+        }
+
+        public string FertNote
+        {
+            get { return fertNote; }
+            set { fertNote = value; }
+        }
+
+    }
+}
---
+++ Program/farmingprogram/Field.cs
@@ -0,0 +1,57 @@
+﻿//Coded by SID 13352132
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsProgram
+{
+    class Fields
+    {
+        //Attributes
+        public int FieldID;
+        private string FieldName;
+        private string FieldNotes;
+        private string FieldStatus;
+
+        //declaring properties
+        public Fields(int fieldID, string fieldname, string fieldnotes,string Fieldstatus)
+
+    {
+        //constructors
+        this.FieldID = fieldID;
+        this.FieldName = fieldname;
+        this.FieldNotes = fieldnotes;
+        this.FieldStatus = Fieldstatus;
+
+        //properties
+    }
+        public int fieldID
+    {
+        get { return FieldID; }
+        set { fieldID = value;}
+
+    }
+        public string fieldName
+        {
+            get { return FieldName; }
+            set { fieldName = value; }
+
+        }
+
+        public string fieldnotes
+        {
+            get { return FieldNotes; }
+            set { fieldnotes = value; }
+
+        }
+
+        public string fieldstatus
+        {
+            get { return FieldStatus; }
+            set { fieldstatus = value; }
+
+        }
+    }
+}
